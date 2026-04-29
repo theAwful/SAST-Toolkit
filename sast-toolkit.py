84022b34c69eac28764553f666a671dd53a08ea5
the_Awful <68723064+theAwful@users.noreply.github.com>
2026-04-28 22:23:02 -0500

    Simplify Bearer installation command

--- sast-toolkit.py
+++ sast-toolkit.py
@@ -55,8 +55,7 @@ def install_bearer():
         return
 
     run(
-        "curl -sfL https://raw.githubusercontent.com/Bearer/bearer/main/contrib/install.sh "
-        "| sh -s -- -b /usr/local/bin"
+        "curl -sfL https://raw.githubusercontent.com/Bearer/bearer/main/contrib/install.sh | sh "
     )
 
 def install_gitleaks():
